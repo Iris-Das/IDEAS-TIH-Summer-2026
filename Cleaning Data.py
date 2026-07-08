dbb9aa70dc0fbf6977a48fbee8af1bb7f6148a41
Iris Das <idasirisdas@gmail.com>
2026-07-08 20:09:39 +0530

    Add files via upload

---
+++ Cleaning Data.py
@@ -0,0 +1,10 @@
+# Change directory to working directory where files are stored
+from contextlib import chdir
+chdir('C:/Users/idasi/Documents/IDEAS-TIH')
+         
+# Import libraries
+import xarray as xr
+import netCDF4 as nc
+import pyarrow as pa
+
+# 
